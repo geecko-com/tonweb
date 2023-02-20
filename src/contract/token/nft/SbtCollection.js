fbc6b5598fc5b357529e90f2cc383a785b02c0e3
Aleksei Zarubin <12220926+alexzarbn@users.noreply.github.com>
2023-02-20 09:39:15 +0100

    feat: add SbtCollection and SbtItem contracts (wip)

--- src/contract/token/nft/SbtCollection.js
+++ src/contract/token/nft/SbtCollection.js
@@ -6,13 +6,16 @@ class SbtCollection extends NftCollection {
      * params   {{itemIndex: BN|number, amount: BN, itemOwnerAddress: Address, authorityAddress :Address, itemContentUri: string, queryId?: number}}
      * @return {Cell}
      */
-   createMintBody(params) {
-       let body = super.createMintBody(params);
+    createMintBody(params) {
+        let body = super.createMintBody(params);
 
-       body.bits.writeAddress(params.authorityAddress);
+        const sbtItemAuthorityAddress = new Cell();
+        sbtItemAuthorityAddress.bits.writeAddress(params.authorityAddress);
 
-       return body;
-   }
+        body.refs[0].refs[0].refs[0] = sbtItemAuthorityAddress;
+
+        return body;
+    }
 }
 
 module.exports = {SbtCollection};
